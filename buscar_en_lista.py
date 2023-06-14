78757f551ffeb0a07f0d19f0d1a0bb4561c906ed
Jose David <Jd>
2023-06-14 08:36:53 +0200

    buscar modo facil

---
+++ buscar_en_lista.py
@@ -0,0 +1,12 @@
+viajes = [
+          ["Toledo", "Ciudad Real", "Cuenca"],
+          ["Paris", "Roma", "Lisboa"],
+          ["Las vegas", "San Francisco", "Lepe"] 
+]
+#sales de Toledo posicion 0
+#Lista 1 en avion y alli paradas de bus
+#lista 2 avion y alli parada de bus
+print(viajes[0][2])
+#salida y 2 paradas de bus a cuenca
+print(viajes[2][2])
+#salida posicion 0 , 2 aviones y 2 paradas de bus "pero merecio la pena"
